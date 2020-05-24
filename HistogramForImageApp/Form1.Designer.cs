e4c67ac446ebc60bbed97a900bee4f147979695d
kalitawojciech <wojtek1311997@gmail.com>
2020-05-24 18:12:08 +0200

    working on project

--- HistogramForImageApp/Form1.Designer.cs
+++ HistogramForImageApp/Form1.Designer.cs
@@ -30,34 +30,125 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.button1.Location = new System.Drawing.Point(29, 32);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(287, 77);
+            this.button1.Size = new System.Drawing.Size(287, 72);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Choose image";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 163);
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(504, 77);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(287, 22);
+            this.textBox1.Size = new System.Drawing.Size(272, 26);
             this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(383, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Threads";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(504, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(272, 26);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(29, 125);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(287, 81);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Process Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(383, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Image Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(383, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Time";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(504, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(272, 26);
+            this.textBox2.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +157,12 @@
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- HistogramForImageApp/Form1.cs
+++ HistogramForImageApp/Form1.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HistogramForImageApp
 {
     public partial class Form1 : Form
     {
+        private Bitmap bitmap;
+        string lastSavedImageTime;
+
         public Form1()
         {
             InitializeComponent();
@@ -16,31 +25,217 @@ namespace HistogramForImageApp
         {
             string filePath = string.Empty;
 
-            using(OpenFileDialog openFileDialog = new OpenFileDialog())
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
             {
-                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.InitialDirectory = @"C:\Users\wojte\Desktop";
                 openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Filter = "Image files (*.jpg)|*.jpg|Image files (*.png)|*.png|All Files (*.*)|*.*";
+                openFileDialog.RestoreDirectory = false;
 
-                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     filePath = openFileDialog.FileName;
-                    textBox1.Text = filePath;
+
 
                     if (openFileDialog.FileName.EndsWith("jpg") || openFileDialog.FileName.EndsWith("png"))
                     {
-                        var bitMap = new Bitmap(filePath);
-                        //textBox1.Text = (color.R).ToString() + " " + (color.G).ToString() + " " + (color.B).ToString();
-                        Image image = (Image)bitMap;
-                        ImageConverter imageConverter = new ImageConverter();
-                        Color color = bitMap.GetPixel(0, 0);
-                        byte[] bytes = (byte[])imageConverter.ConvertTo(image, typeof(byte[]));
-                        //textBox1.Text = bytes[0].ToString() + " " + bytes[1].ToString() + " " + bytes[2].ToString();
-                        //textBox1.Text = (color.R).ToString() + " " + (color.G).ToString() + " " + (color.B).ToString();
-                        textBox1.Text = image.PixelFormat.ToString();
+                        bitmap = new Bitmap(filePath);
+                        int position = filePath.LastIndexOf("\\") + 1;
+                        textBox1.Text = filePath.Substring(position, filePath.Length - position);
                     }
                 }
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (bitmap != null)
+            {
+                ProcessImage();
+            }
+        }
+
+        private void ProcessImage()
+        {
+            this.button2.Enabled = false;
+
+            int threadsNumber = (int)numericUpDown1.Value;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            
+            ConcurrentDictionary<Color, int> colors = new ConcurrentDictionary<Color, int>();
+            //List<Dictionary<Color, int>> colors = new List<Dictionary<Color, int>>();
+            //for(int i = 0; i < threadsNumber; i++)
+            //{
+            //    colors.Add(new Dictionary<Color, int>());
+            //}
+
+            int heightJump = bitmap.Height / threadsNumber;
+
+            int moduloHeight = bitmap.Height % threadsNumber;
+            int bitmapWidth = bitmap.Width;
+
+            int[] heightBreakpoints = new int[threadsNumber + 1];
+            heightBreakpoints[0] = 0;
+
+            for (int i = 2; i < threadsNumber + 2; i++)
+            {
+                heightBreakpoints[i - 1] = heightJump * (i - 1);
+            }
+
+            if (moduloHeight != 0)
+            {
+                heightBreakpoints[threadsNumber] = heightBreakpoints[threadsNumber] + moduloHeight;
+            }
+
+
+            Parallel.For(0, threadsNumber,
+                index =>
+                {
+
+                    int heightStartIndex = heightBreakpoints[index];
+                    int widthLimit = bitmapWidth;
+                    int heightLimit = heightBreakpoints[index + 1];
+
+                    for (int i = 0; i < widthLimit; i++)
+                    {
+                        for (int j = heightStartIndex; j < heightLimit; j++)
+                        {
+                            System.Drawing.Color pixel;
+                            lock (bitmap)
+                            {
+                                pixel = bitmap.GetPixel(i, j);
+                            }
+
+                            //if (colors[index].ContainsKey(pixel))
+                            //{
+                            //    colors[index][pixel]++;
+                            //}
+                            //else
+                            //{
+                            //    colors[index][pixel] = 1;
+                            //}
+
+                            if (colors.ContainsKey(pixel))
+                            {
+                                colors[pixel]++;
+                            }
+                            else
+                            {
+                                colors[pixel] = 1;
+                            }
+                        }
+                    }
+            });
+
+
+            //Dictionary<Color, int> allColors = new Dictionary<Color, int>();
+            //foreach(var colorDictionary in colors)
+            //{
+            //    AddDictionaryToDictionary(allColors, colorDictionary);
+            //}
+
+            Dictionary<Color, int> allColors = colors.ToDictionary(x => x.Key, x => x.Value);
+
+            List<KeyValuePair<Color, int>> colorsList = allColors.ToList();
+
+            var allColorsTemp = (from color in allColors orderby color.Value descending select color).Take(10);
+
+
+
+            sw.Stop();
+
+            int sumOfPixels = CountSumOfPixelsInDictionary(allColors);
+
+            allColors = allColorsTemp.ToDictionary(x => x.Key, x => x.Value);
+
+            var countsStrBuilder = new StringBuilder();
+            countsStrBuilder.AppendLine("RGB COUNTS");
+            countsStrBuilder.AppendLine("R, G, B, Counts");
+            PrintColors(allColors, countsStrBuilder);
+
+            countsStrBuilder.AppendLine("Total time, Number of threads");
+            countsStrBuilder.AppendLine(String.Format("{0}, {1}", sw.Elapsed + "s", threadsNumber.ToString()));
+            countsStrBuilder.AppendLine("Image width, height");
+            countsStrBuilder.AppendLine(String.Format("{0}, {1}", bitmap.Width, bitmap.Height));
+            countsStrBuilder.AppendLine("Image * height");
+            countsStrBuilder.AppendLine(String.Format("{0}", bitmap.Width * bitmap.Height));
+            lastSavedImageTime = sw.Elapsed.ToString();
+
+            countsStrBuilder.AppendLine("sum of pixels");
+            countsStrBuilder.AppendLine(String.Format("{0}", sumOfPixels));
+
+            this.textBox2.Text = lastSavedImageTime;
+
+            SaveCsv(@"C:\Users\wojte\Desktop\imageRaport\", countsStrBuilder, "rgba.csv");
+
+            this.button2.Enabled = true;
+        }
+
+        private void PrintColors(Dictionary<Color, int> colors, StringBuilder stringBuilder)
+        {
+            foreach (var color in colors)
+            {
+                stringBuilder.AppendLine(String.Format("{0},{1},{2}, {3}", color.Key.R, color.Key.G, color.Key.B, color.Value));
+            }
+        }
+
+        private void SaveCsv(string path, StringBuilder strBuilder, string fileName)
+        {
+            string pathToSave = path + fileName;
+
+            if (File.Exists(pathToSave))
+            {
+                File.Delete(pathToSave);
+            }
+
+            File.AppendAllText(pathToSave, strBuilder.ToString());
+        }
+
+
+        private void AddDictionaryToDictionary(Dictionary<Color, int> mainDictionary, Dictionary<Color, int> dictionaryToAdd)
+        {
+            foreach (KeyValuePair<Color, int> entry in dictionaryToAdd)
+            {
+                if (mainDictionary.ContainsKey(entry.Key))
+                {
+                    mainDictionary[entry.Key] += entry.Value;
+                }
+                else
+                {
+                    mainDictionary[entry.Key] = entry.Value;
+                }
+            }
+        }
+
+        private Dictionary<Color, int> AddDictionaryTwoDictionaries(Dictionary<Color, int> mainDictionary, Dictionary<Color, int> dictionaryToAdd)
+        {
+            Dictionary<Color, int> result = mainDictionary;
+            foreach (KeyValuePair<Color, int> entry in dictionaryToAdd)
+            {
+                if (result.ContainsKey(entry.Key))
+                {
+                    result[entry.Key] += entry.Value;
+                }
+                else
+                {
+                    result[entry.Key] = entry.Value;
+                }
+            }
+
+            return result;
+        }
+
+
+        private int CountSumOfPixelsInDictionary(Dictionary<Color, int> colors)
+        {
+            int sumOfPixels = 0;
+            foreach (KeyValuePair<Color, int> entry in colors)
+            {
+                sumOfPixels += entry.Value;
+            }
+
+            return sumOfPixels;
         }
     }
 }
